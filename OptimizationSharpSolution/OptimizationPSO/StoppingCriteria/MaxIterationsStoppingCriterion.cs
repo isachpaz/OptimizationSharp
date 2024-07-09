05d4d5f672df372b8797007415602007da43a4c9
Ilias Sachpazidis <ilias.sachpazidis@gmail.com>
2024-07-09 10:29:02 +0200

    MaxIterationsStoppingCriterion added

---
+++ OptimizationSharpSolution/OptimizationPSO/StoppingCriteria/MaxIterationsStoppingCriterion.cs
@@ -0,0 +1,27 @@
+﻿using OptimizationPSO.Swarm;
+
+namespace OptimizationPSO.StoppingCriteria
+{
+    public class MaxIterationsStoppingCriterion : BaseStoppingCriterion
+    {
+        private readonly long _maxIterations;
+        public static long Iterations = 0;
+
+        public MaxIterationsStoppingCriterion(long maxIterations)
+            : base($"MaxIterations={maxIterations}")
+        {
+            _maxIterations = maxIterations;
+        }
+
+        public override bool CanStop(ParticleSwarm particleSwarm)
+        {
+            Iterations++;
+            if (Iterations >= _maxIterations)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
